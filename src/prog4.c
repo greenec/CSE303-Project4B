9620df6c345bbb91e17381d397f5d479bd1e8640
lehigh123 <domschris@gmail.com>
2020-04-16 19:21:36 +0000

    cli for threads

--- src/prog4.c
+++ src/prog4.c
@@ -10,6 +10,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+
+#include <string.h>
+#include <unistd.h>
+
 //
 // Lehigh University
 // CSE 303 Spring 2020
@@ -20,6 +25,8 @@
 //
 void *  getVals( int argc,  char ** argv);
 int determineMode(int argc, char** argv);
+int getNumberOfThreads(int argc, char** argv);
+void printUseage();
 
 int main( int argc,  char ** argv)
 {
@@ -27,8 +34,30 @@ int main( int argc,  char ** argv)
    for(int i = 0 ; i < argc; i++){
       //printf("%s\n", argv[i]);
    }
-   int retval = determineMode(argc, argv);
-   printf("%d\n", retval);
+   int mode = determineMode(argc, argv);
+
+   if(mode != 1 && mode != 2){
+      printUseage();
+      return 0;
+   }
+
+   if(mode == 2){
+      printf("%s\n", "Spin lock test" );
+      //call spin lock 
+   } else if (mode == 1){
+      printf("%s\n", "Threaded merge sort" );
+      int threads = getNumberOfThreads(argc, argv);
+      printf("Threads: %d \n", threads );
+      //parse more args 
+   } else{
+      //should never be here retval can only ever be 1 or 2
+      printf("%s \n", "Error parsing CLI.");
+      return 0;
+   }
+
+
+
+
    /*
    void *  v = getVals(argc, argv);
       
@@ -69,44 +98,60 @@ int main( int argc,  char ** argv)
    return 1;
 }
 
+int getNumberOfThreads(int argc, char** argv){
+   int threads = -1;
+   int opt;
+   while ((opt = getopt(argc, argv, "n:")) != -1) {
+        switch (opt) {
+            case 'n':
+               threads = atoi(optarg);
+               break;
+            case 'p':
+               return 
+            default :  
+               continue;
+        }
+   }
+   return threads;
+}
+
+/*
+char* getFileName(int argc, char** argv){
+   char* fileName = NULL;
+   return fileName;
+}
+*/
+
+
+
 int determineMode(int argc, char** argv){
    /*
    if t == 1 then run merge sort
    if t == 2 then run spin lock
    otherwise return 0 and exit 
    */
-
    int mode = 0;
    if(argc <= 1){
+      printUseage();
       return 0;
    }
 
-   int c;
-   char* cvalue = NULL;
-   while ((c = getopt (argc, argv, "tnfp:")) != -1){
-     switch (c) {
-      case 't':
-         cvalue = optarg;
-         //mode = atoi(cvalue);
-         fprintf(stderr, "%s\n", cvalue);
-         if(mode == 0){
-            //printf("Usage: <program name> ( -tx {-ny}| -p ){-f filename} i \n");
-            //printf("Goodbye. \n");
-            //exit(1);
-            //return mode;
-         }
-         break;
-      default:
-         printf("Usage: <program name> ( -tx {-ny}| -p ){-f filename} i \n");
-         printf("Goodbye. \n");
-         exit(1);
-      }
+   int opt;
+   while ((opt = getopt(argc, argv, "t:")) != -1) {
+        switch (opt) {
+          case 't': 
+            mode = atoi(optarg);
+            return mode;
+          default : printUseage(); return 0;
+        }
    }
-   
-return mode;
+   return mode;
+}
 
 
 
+void printUseage(){
+   printf("%s\n", "Usage: <program name> ( -tx {-ny}| -p ){-f filename} i" );
 }
 
 
