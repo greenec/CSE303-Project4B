c1adee4857a89936e1b717acf05d9623d0cf74da
lehigh123 <domschris@gmail.com>
2020-04-16 20:13:48 +0000

    CLI is complete

--- src/prog4.c
+++ src/prog4.c
@@ -29,12 +29,13 @@ typedef struct Commands {
    char* filename;
    int userEntry;
    int messages;
+   int stdout;
 } Commands;
 
-//
+
 void *  getVals( int argc,  char ** argv);
 void determineMode(int argc, char** argv, struct Commands*);
-int getNumberOfThreads(int argc, char** argv);
+int validateArguments(struct Commands*);
 void printUseage();
 
 
@@ -51,11 +52,16 @@ int main( int argc,  char ** argv)
    determineMode(argc, argv, command);
    printf(" Mode: %d \n", command->mode  );
    printf(" Threads: %d \n", command->threads  );
-   printf(" Mode: %d \n", command->userEntry  );
+   printf(" Messages: %d \n", command->messages  );
    printf(" Mode: %d \n", command->messages  );
+   printf(" Filename: %s \n", command->filename  );
 
 
-
+   int valid = validateArguments(command);
+   if(!valid){
+      printUseage();
+      return 0;
+   }
 
 
 
@@ -101,6 +107,49 @@ int main( int argc,  char ** argv)
    return 1;
 }
 
+int validateArguments(struct Commands* command){
+   if(command->mode == 1){
+      if(command->userEntry != 0){
+         return 0;
+      }
+
+      if(command->threads != 0){
+         return 0;
+      }
+      return 1;
+   } else if(command->mode == 2){
+      if(command->threads < 1 || command->threads > 4){
+         return 0;
+      } else{
+         return 1;
+      }
+   } else{
+      if(command->userEntry == 1){
+         printf("Enter mode: "); 
+         scanf("%d", &command->mode);
+         if(command->mode == 2){
+            printf("Enter number of threads: "); 
+            scanf("%d", &command->threads);
+            if(command->threads > 1 && command->threads < 5){
+               return 1;
+            }
+         }  else if(command->mode == 1 ){
+            return 1;
+         }
+         else{
+            return 0;
+         }
+      } else{
+         return 0;
+      }
+   }
+
+   if(command->messages <= 0){
+      return 0;
+   }
+   return 1;
+}
+
 
 
 
@@ -125,14 +174,15 @@ void determineMode(int argc, char** argv, struct Commands* command){
             case 'n':
                command->threads = atoi(optarg);
                break;
-            case 'f':
-               command->filename = optarg;
-               break;
             case 'p':
                command->userEntry = 1;
                return;
             case 'i':
                command->messages = atoi(optarg);
+               break;
+            case 'f':
+               command->filename = optarg;
+               command->stdout = 1;
                break;
             default : printUseage(); return;
         }
